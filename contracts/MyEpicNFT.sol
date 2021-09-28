45b9a80319665b98a520c6555c86faf2b8e80ed2
m5r <mokht@rmi.al>
2021-09-28 23:32:43 +0200

    mind random valdax

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -3,22 +3,70 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+import { Base64 } from "./libraries/Base64.sol";
 
 contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] valdaxs = ["Valdapapax", "Vallacrimax", "Valnibbalax", "Valmuradinax", "Valguilumax", "Valskywalkax"];
+
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Woah!");
     }
 
+    function pickRandomValdax(uint tokenId) public view returns (string memory) {
+        uint rand = random(string(abi.encodePacked("valdax", Strings.toString(tokenId))));
+        rand = rand % valdaxs.length;
+        return valdaxs[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint) {
+        return uint(keccak256(abi.encodePacked(input)));
+    }
+
     function makeAnEpicNFT() public {
         uint newItemId = _tokenIds.current();
+
+        string memory valdax = pickRandomValdax(newItemId);
+        string memory finalSvg = string(abi.encodePacked(baseSvg, valdax, "</text></svg>"));
+
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        valdax,
+                        '", "description": "Your very own Valdax.", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/21WT");
-        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        // Update your URI!!!
+        _setTokenURI(newItemId, finalTokenUri);
+
         _tokenIds.increment();
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     }
 }
